a44e844c4b796af48a0076bed0a30f7aa7eae9a9
Liraz Siri <liraz@turnkeylinux.org>
2010-05-09 07:26:23 +0300

    implemented useraufs.conf limitations
    * logic
        /etc/useraufs.conf: configuration file that determines security limitations for useraufs
    
        if no configuration file: all users and all paths allowed by default
            so long as they follow policy
    
    * format
        # comment
    
        allow_dir /path/to/allowed/parent/dir
        allow_user z

--- pylib/useraufs.py
+++ pylib/useraufs.py
@@ -7,7 +7,42 @@ class Error(Exception):
     pass
 
 class UserAufs:
+    CONF_FILE="/etc/useraufs.conf"
+    
+    def _load_conf(self):
+        self.allowed_uids = [] # empty array means allow all
+        self.allowed_dirs = [] # empty array means allow all
+
+        if not os.path.exists(self.CONF_FILE):
+            return
+        
+        for line in file(self.CONF_FILE).readlines():
+            line = line.strip()
+            
+            if not line or line.startswith("#"):
+                continue
+
+            op, val = re.split(r'\s+', line, 1)
+            if op == 'allow_user':
+                self.allowed_uids.append(pwd.getpwnam(val).pw_uid)
+            elif op == 'allow_dir':
+                if not val.startswith('/'):
+                    raise Error("relative paths are illegal")
+
+                # I think its better to just ignore non-existent paths
+                if not os.path.exists(val):
+                    continue
+
+                if not os.path.isdir(val):
+                    raise Error("not a directory: '%s'" % val)
+                
+                self.allowed_dirs.append(os.path.realpath(val))
+            else:
+                raise Error("illegal configuration line: " + line)
+        
     def __init__(self):
+        self._load_conf()
+            
         uid = os.getuid()
         euid = os.geteuid()
 
@@ -17,9 +52,13 @@ class UserAufs:
         if uid == 0:
             raise Error("useraufs may not be used by root")
 
+        username = pwd.getpwuid(uid).pw_name
+        if self.allowed_uids and uid not in self.allowed_uids:
+            raise Error("configuration does not allow user %s" % username)
+
         self.uid = uid
         self.euid = euid
-        self.username = pwd.getpwuid(uid).pw_name
+        self.username = username
 
     def _system(self, command):
         os.setuid(self.euid)
@@ -30,16 +69,31 @@ class UserAufs:
             raise Error("failed exitcode (%d): %s" % (os.WEXITSTATUS(error), command),
                         error)
 
+    def _check_is_dir_ok(self, dir):
+        if not os.path.isdir(dir):
+            raise Error("not a directory: %s" % dir)
+
+        if os.lstat(dir).st_uid != self.uid:
+            raise Error("directory '%s' is not owned by user %s" % (dir, self.username))
+
+        if self.allowed_dirs:
+            def is_dir_allowed(dir):
+                dir = os.path.realpath(dir)
+                for allowed_dir in self.allowed_dirs:
+                    if dir == allowed_dir or dir.startswith(allowed_dir + '/'):
+                        return True
+
+                return False
+
+            if not is_dir_allowed(dir):
+                raise Error("configuration disallows operations involving directory '%s'" % dir)
+
     def mount(self, branches, mnt):
         dirs = [ re.sub('=.*', '', branch.strip()) for branch in branches.split(':') ]
         dirs.append(mnt)
 
         for dir in dirs:
-            if not os.path.isdir(dir):
-                raise Error("not a directory: %s" % dir)
-
-            if os.lstat(dir).st_uid != self.uid:
-                raise Error("directory '%s' is not owned by user %s" % (dir, self.username))
+            self._check_is_dir_ok(dir)
 
         options = "dirs=" + branches
         command = "mount -t aufs -o %s none %s" % (utils.mkarg(options),
@@ -52,11 +106,7 @@ class UserAufs:
         dirs.append(mnt)
 
         for dir in dirs:
-            if not os.path.isdir(dir):
-                raise Error("not a directory: %s" % dir)
-
-            if os.lstat(dir).st_uid != self.uid:
-                raise Error("directory '%s' is not owned by user %s" % (dir, self.username))
+            self._check_is_dir_ok(dir)
 
         options = "remount," + operations
         command = "mount -o %s %s" % (utils.mkarg(options),
@@ -64,8 +114,7 @@ class UserAufs:
         self._system(command)
 
     def umount(self, mnt):
-        if os.lstat(mnt).st_uid != self.uid:
-            raise Error("directory '%s' is not owned by user %s" % (mnt, self.username))
+        self._check_is_dir_ok(mnt)
 
         command = "umount " + utils.mkarg(mnt)
         self._system(command)

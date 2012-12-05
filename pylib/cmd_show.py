00bc93d69840a2b70f1038e97ad09c206e9a32e6
Alon Swartz <alon@turnkeylinux.org>
2012-12-05 13:36:21 +0200

    updated copyright/license

--- pylib/cmd_show.py
+++ pylib/cmd_show.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python
+# Copyright (c) TurnKey Linux - http://www.turnkeylinux.org
+#
+# This file is part of UserAUFS
+#
+# UserAUFS is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation; either version 3 of the License, or (at your
+# option) any later version.
+
 """Show this user's union mounts
 Options:
 	-m	print multi-line format (more human readable)
--- pylib/utils.py
+++ pylib/utils.py
@@ -1,4 +1,12 @@
-# Copyright (c) 2010 Liraz Siri <liraz@turnkeylinux.org> - all rights reserved
+# Copyright (c) TurnKey Linux - http://www.turnkeylinux.org
+#
+# This file is part of UserAUFS
+#
+# UserAUFS is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation; either version 3 of the License, or (at your
+# option) any later version.
+
 import os
 import sys
 import types
